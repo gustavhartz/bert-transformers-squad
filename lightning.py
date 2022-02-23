2136cff195271b7d9a88ca88b1bea03401ec9f46
gustav hartz <gustav.s.hartz@gmail.com>
2022-02-23 13:50:19 +0000

    Better logging and multi gpu

--- lightning.py
+++ lightning.py
@@ -1,6 +1,7 @@
 
 import torch
 import pytorch_lightning as pl
+import wandb
 # simple pytorch training loop using tqdm to show progress
 
 
@@ -9,6 +10,7 @@ class PLQAModel(pl.LightningModule):
         super().__init__()
         self.hparams.update(hparams)
         self.model = model
+        self.save_hyperparameters()
         
     def forward(self, x):
         # in lightning, forward defines the prediction/inference actions
@@ -28,7 +30,8 @@ class PLQAModel(pl.LightningModule):
                              start_positions=start_positions, end_positions=end_positions)
         loss = outputs[0]
         self.log("train_loss", loss)
-        return loss
+
+        return {"loss": loss, "outputs":outputs[1]}
 
     def validation_step(self, batch, batch_idx):
         input_ids = batch['input_ids']
@@ -43,7 +46,22 @@ class PLQAModel(pl.LightningModule):
             loss,
             on_epoch=True
         )
-        return loss
+        # Log text examples
+        return {"loss": loss, "outputs":outputs[1]}
+
+    def training_epoch_end(self, outputs):
+        ct, _sum = 0, 0
+        for pred in outputs:
+            _sum += pred["loss"]
+            ct += 1
+        return _sum / ct
+    
+    def validation_epoch_end(self, outputs):
+        ct, _sum = 0, 0
+        for pred in outputs:
+            _sum += pred["loss"]
+            ct += 1
+        return _sum / ct
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.hparams.lr)
--- main.py
+++ main.py
@@ -7,12 +7,37 @@ import torch
 from data import SquadDataset
 from torch.utils.data import DataLoader
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import Callback
+from transformers import BertTokenizerFast
 
+# Not tested
+class LogTextSamplesCallback(Callback):
+    def on_training_batch_end(
+            self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+            """Called when the validation batch ends."""
+
+            # `outputs` comes from `LightningModule.validation_step`
+            # which corresponds to our model predictions in this case
+
+            # Let's log 20 sample image predictions from first batch
+            if batch_idx == 0:
+                wandb_logger=trainer.logger
+                tokenizer = pl_module.tokenizer
+                questions = [tokenizer.decode(x['input_ids'][x['token_type_ids'].nonzero().squeeze()]) for x in batch]
+                answers = [tokenizer.decode(x['input_ids'][x['start_positions']:x['end_positions']]) for x in batch]
+                start_pred = torch.argmax(outputs[1], dim=1)
+                end_pred = torch.argmax(outputs[2], dim=1)
+                preds = [tokenizer.decode(x[2][x[0]:x[1]]) if x[0]<x[1] else "fes" for x in zip(start_pred,end_pred, batch['input_ids']) ]
+                # Option 2: log predictions as a Table
+                columns = ['question', 'answer', 'prediction']
+                data = [questions,answers,preds]
+                wandb_logger.log_text(key='traning_predicition_sample', columns=columns, data=data)
 
 def main():
+    global hparams
     hparams = {
         'lr': 5e-5,
-        'batch_size': 14,
+        'batch_size': 16,
         'num_workers': 4,
         'num_labels': 2,
         'hidden_size': 768,
@@ -23,18 +48,19 @@ def main():
     val_encodings = torch.load("./squad/val_encodings")
     train_dataset = SquadDataset(train_encodings)
     val_dataset = SquadDataset(val_encodings)
+    
+    wandb_logger = WandbLogger(project="bert-squad", entity="gustavhartz")
+    
+    print("Batch size", hparams.get("batch_size"))
     train_loader = DataLoader(
-        train_dataset, batch_size=hparams.get("batch_size"), shuffle=True)
+        train_dataset, batch_size=hparams.get("batch_size"), shuffle=True, num_workers=hparams.get('num_workers'))
     val_loader = DataLoader(
-        val_dataset, batch_size=hparams.get("batch_size"), shuffle=True)
-
+        val_dataset, batch_size=hparams.get("batch_size"), shuffle=True, num_workers=hparams.get('num_workers'))
+    
     del train_encodings
     del val_encodings
     model = QAModelBert(hparams, hparams['bert_model'])
-    litModel = PLQAModel(model, hparams)
-    # Logger
-    wandb_logger = WandbLogger(project="bert-squad", entity="gustavhartz")
-    
+    litModel = PLQAModel(model, hparams)    
     trainer = pl.Trainer(gpus=2, max_epochs=hparams['num_train_epochs'], logger=wandb_logger, strategy='dp')
     trainer.fit(litModel, train_loader, val_loader)
     torch.save(model.model, "model.model")
